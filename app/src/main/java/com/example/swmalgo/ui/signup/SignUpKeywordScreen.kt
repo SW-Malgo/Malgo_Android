c9a55c3bea3da7dd71ec2784f0f2565624ff0037
JayKim0202 <jayjkim0223@gmail.com>
2023-06-30 03:41:10 +0900

    keyword

--- app/src/main/java/com/example/swmalgo/ui/signup/SignUpKeywordScreen.kt
+++ app/src/main/java/com/example/swmalgo/ui/signup/SignUpKeywordScreen.kt
@@ -2,12 +2,14 @@ package com.example.swmalgo.ui.signup
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -22,6 +24,10 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -48,6 +54,7 @@ import com.example.swmalgo.ui.theme.White800
 import com.example.swmalgo.utils.Constants.MAIN_GRAPH
 import com.example.swmalgo.utils.Constants.SIGNUP_GRAPH
 
+
 @Composable
 fun SignUpKeywordScreen(
     appState: ApplicationState = rememberApplicationState(),
@@ -63,7 +70,9 @@ fun SignUpKeywordScreen(
     ) {
         Column(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .verticalScroll(rememberScrollState())
                 .padding(horizontal = 30.dp)
         ) {
             Text(
@@ -122,7 +131,11 @@ fun SignUpKeywordScreen(
                 )
             }
 
-            KeywordBox()
+            KeywordBox(
+//                showSnackBar = {
+//                    appState.showSnackbar("3")
+//                }
+            )
         }
 
         Button(
@@ -155,68 +168,96 @@ fun SignUpKeywordScreen(
 private fun KeywordBox() {
     val configuration = LocalConfiguration.current
 
-    val sw = configuration.screenWidthDp
-    val sh = configuration.screenHeightDp
 
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .verticalScroll(rememberScrollState())
-    ) {
-        Box(
-            modifier = Modifier
-                .padding(top = 12.dp)
-                .fillMaxWidth()
-                .wrapContentHeight()
-        ) {
-            Box(
-                modifier = Modifier
-                    .offset(x = (sw * 0.6).toInt().dp, y = (sh * 0.1).toInt().dp)
-                    .size(115.dp)
-                    .clip(RoundedCornerShape(80))
-                    .background(POINT.copy(alpha = 0.5f))
-            ) {
-                Text(
-                    text = "#오픈소스",
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = PURE_WHITE,
-                    modifier = Modifier
-                        .align(Alignment.Center)
-                )
-            }
-
-            Box(
-                modifier = Modifier
-                    .offset(x = (sw * 0.8).toInt().dp, y = (sh * 0.3).toInt().dp)
-                    .size(165.dp)
-                    .clip(RoundedCornerShape(80))
-                    .background(POINT.copy(alpha = 0.5f))
-            ) {
-                Text(
-                    text = "#AI",
-                    fontSize = 38.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = PURE_WHITE,
-                    modifier = Modifier
-                        .align(Alignment.Center)
-                )
-            }
-
-
-
-
-
-
-
-
-        }
-
-
-
-
-
-
+    var keywordList by remember {
+        mutableStateOf(listOf<String>())
     }
+
+    Box(
+        modifier = Modifier
+            .padding(top = 12.dp)
+            .fillMaxWidth()
+            .height(780.dp)
+    ) {
+        val itemList: List<List<Any>> = listOf(
+            listOf(240, 85, 115, "#오픈소스", 16),
+            listOf(0, 68, 165, "#AI", 38),
+            listOf(132, 214, 115, "#부동산", 24),
+            listOf(-17, 271, 137, "#밴드", 24),
+            listOf(120, 407, 86, "#등산", 12),
+            listOf(231, 290, 154, "#운동", 24),
+            listOf(240, 466, 115, "#투자", 24),
+            listOf(10, 508, 190, "#음악", 36),
+            listOf(200, 609, 115, "#코딩", 20)
+        )
+
+        var index = 0
+        itemList.forEach { item ->
+            val sw = itemList[index][0] as Int
+            val sh = itemList[index][1] as Int
+            val cs = itemList[index][2] as Int
+            val ht = itemList[index][3] as String
+            val fs = itemList[index][4] as Int
+
+            RoundCheckbox(sw, sh, cs, ht, fs,
+                listSize = keywordList.size,
+            onclick = { isChecked->
+                if(isChecked){
+                    keywordList = keywordList.toMutableList().apply {
+                        add(ht)
+                    }
+                }else{
+                    keywordList = keywordList.toMutableList().apply {
+                        remove(ht)
+                    }
+                }
+
+            }
+            )
+            index += 1
+        }
+    }
+}
+@Composable
+fun RoundCheckbox(
+    sw: Int,
+    sh: Int,
+    cs: Int,
+    ht: String,
+    fs: Int,
+    onclick: (Boolean) -> Unit,
+    listSize: Int
+) {
+    var isChecked by remember {
+        mutableStateOf(false)
+    }
+    Box(
+        modifier = Modifier
+            .offset(x = (sw).toInt().dp, y = (sh).toInt().dp)
+            .size(cs.dp)
+            .clip(RoundedCornerShape(80))
+            .clickable {
+                if (listSize > 3) {
+                    if (isChecked) {
+                        isChecked = !isChecked
+                    } else{
+                    }
+                } else {
+                    isChecked = !isChecked
+                    onclick(isChecked)
+                }
+            }
+            .background(if (isChecked) POINT.copy(alpha = 0.5f) else Color(0xff737373).copy(alpha = 0.5f))
+    ) {
+        Text(
+            text = ht,
+            fontSize = fs.sp,
+            fontWeight = FontWeight.Bold,
+            color = PURE_WHITE,
+            modifier = Modifier
+                .align(Alignment.Center)
+        )
+    }
+
+
 }
