1da87a2cebabc885b45dcc87d73b852b68a1c3c7
JayKim0202 <jayjkim0223@gmail.com>
2023-06-29 09:52:09 +0900

    .

--- app/src/main/java/com/example/swmalgo/KeywordSelectionActivity.kt
+++ app/src/main/java/com/example/swmalgo/KeywordSelectionActivity.kt
@@ -3,15 +3,29 @@ package com.example.swmalgo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.example.swmalgo.ui.theme.SwMalgoTheme
 
@@ -39,9 +53,47 @@ fun KeywordSplashScreen(modifier: Modifier = Modifier) {
             text = "직장인들의 은밀한 취미생활 malgo!",
             fontSize = 19.sp,
             modifier = Modifier
-                .padding(146.dp)
+                .padding(top = 146.dp)
         )
 
+        Text(
+            text = "관심사 키워드를\n선택해주세요",
+            style = TextStyle(lineHeight = 1.5.em),
+            fontSize = 39.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 21.dp)
+        )
 
+        Row(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(start = 12.dp, top = 36.dp)
+        ){
+            Text(
+                text = "현재 핫한 모임",
+                fontSize = 21.sp,
+            )
+            Image(
+                painter = painterResource(id = R.drawable.ic_fire),
+                contentDescription = null,
+                contentScale = ContentScale.FillHeight, // ScaleType
+                modifier = Modifier
+                    .size(width = 21.dp, height = 25.dp)
+                    .padding(top = 5.dp)
+            )
+
+        }
+
+        Box(
+            modifier = Modifier
+                .padding(top = 62.dp)
+                .weight(1f)
+                .background(Color.LightGray)
+        ){
+            Text(
+                text = "#~~~"
+            )
+        }
     }
 }
